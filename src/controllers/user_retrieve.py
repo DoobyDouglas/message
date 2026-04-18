fe47b216bb3ddd4a97dc79ff71106af3503ad26c
dooby <doobydouglas90@gmail.com>
2026-04-18 23:32:57 +0300

    DEV-0005_20260418_233252

--- src/controllers/user_retrieve.py
+++ src/controllers/user_retrieve.py
@@ -6,6 +6,7 @@
 
 import uuid
 
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.controllers.base import BaseController
@@ -32,8 +33,6 @@ class UserRetrieveController(BaseController):
         """
         service = UserReadService(session)
         if (user := await service.get_user(user_uuid)) is None:
-            from fastapi import HTTPException, status
-
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="Пользователь не найден",
